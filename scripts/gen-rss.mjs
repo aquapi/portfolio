71afa2618c411b2a4f25665be2526c8a5cc51a3d
Revenity <87530663+aquapi@users.noreply.github.com>
2022-04-09 11:32:17 +0700

    Initial commit

--- scripts/gen-rss.mjs
+++ scripts/gen-rss.mjs
@@ -3,25 +3,30 @@ import { join, resolve } from 'path';
 import RSS from 'rss';
 import matter from 'gray-matter';
 
+// Remake the __dirname property
 const __dirname = resolve("./scripts");
 
 const feed = new RSS({
 	title: 'Revenity',
-	site_url: 'https://yoursite.com',
-	feed_url: 'https://yoursite.com/feed.xml'
-})
+	site_url: 'https://portfolio-aquapi.vercel.app',
+	feed_url: 'https://portfolio-aquapi.vercel.app/feed.xml'
+});
 
-const posts = await fs.readdir(join(__dirname, '..', 'pages', 'posts'))
+// Read all the posts
+const posts = await fs.readdir(join(__dirname, '..', 'pages', 'posts'));
 
 await Promise.all(
-	posts.map(async (name) => {
-		if (name.startsWith('index.')) return
+	posts.map(async name => {
+		// Skip index.md
+		if (name.startsWith('index.')) return;
 
+		// Post content
 		const content = await fs.readFile(
 			join(__dirname, '..', 'pages', 'posts', name)
-		)
-		const frontmatter = matter(content)
+		);
+		const frontmatter = matter(content);
 
+		// Add the post to the feed
 		feed.item({
 			title: frontmatter.data.title,
 			url: '/posts/' + name.replace(/\.mdx?/, ''),
@@ -29,8 +34,9 @@ await Promise.all(
 			description: frontmatter.data.description,
 			categories: frontmatter.data.tag.split(', '),
 			author: frontmatter.data.author
-		})
+		});
 	})
 );
 
+// Write the feed with converted XML
 await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }));
