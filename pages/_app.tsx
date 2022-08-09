8e43f6cc84547d6c6399d7a8befdd4a64f53a9ee
Revenity <87530663+aquapi@users.noreply.github.com>
2022-08-09 22:07:41 +0700

    Add head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,21 @@ import "../styles/app.scss";
 import type { AppProps } from "next/app";
 import Loader, { Context } from "../components/utils/Loader";
 import { useContext } from "react";
+import Head from "next/head";
 
 function App({ Component, pageProps }: AppProps) {
 	const ctx = useContext(Context);
 	ctx.addValue("btn-ghost");
 
-	return <Loader>
-		<Component {...pageProps} />
-	</Loader>;
+	return <>
+	<Head>
+		<title>AquaPI</title>
+		<meta name="description" content="I'm AquaPI and this is my portfolio." />
+	</Head>
+		<Loader>
+			<Component {...pageProps} />
+		</Loader>
+	</>;
 }
 
 export default App;
